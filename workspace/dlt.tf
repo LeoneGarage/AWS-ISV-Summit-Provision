712050b58668183de7c5f3d4de31321eb5ee2391
Leon Eller <leon.eller@databricks.com>
2022-04-12 17:37:45 +1000

    Fixed references to Notebooks

--- workspace/dlt.tf
+++ workspace/dlt.tf
@@ -110,22 +110,22 @@ resource "null_resource" "dlt_pipeline_sql_patch_policy_id" {
     "libraries": [
         {
             "notebook": {
-                "path": "/Repos/leoneller@yahoo.com.au/AWS-ISV-Summit/dlt/sql/00_etl_dlt_bronze"
+                "path": "/Repos/${each.key}/AWS-ISV-Summit/dlt/sql/00_etl_dlt_bronze"
             }
         },
         {
             "notebook": {
-                "path": "/Repos/leoneller@yahoo.com.au/AWS-ISV-Summit/utils/udfs"
+                "path": "/Repos/${each.key}/AWS-ISV-Summit/utils/udfs"
             }
         },
         {
             "notebook": {
-                "path": "/Repos/leoneller@yahoo.com.au/AWS-ISV-Summit/dlt/sql/20_etl_dlt_gold"
+                "path": "/Repos/${each.key}/AWS-ISV-Summit/dlt/sql/20_etl_dlt_gold"
             }
         },
         {
             "notebook": {
-                "path": "/Repos/leoneller@yahoo.com.au/AWS-ISV-Summit/dlt/sql/10_etl_dlt_silver"
+                "path": "/Repos/${each.key}/AWS-ISV-Summit/dlt/sql/10_etl_dlt_silver"
             }
         }
     ],
@@ -170,7 +170,7 @@ resource "databricks_pipeline" "dlt_pipeline_python" {
 
   library {
     notebook {
-      path = "/Repos/${each.key}/AWS-ISV-Summit//utils/udfs"
+      path = "/Repos/${each.key}/AWS-ISV-Summit/utils/udfs"
     }
   }
   library {
@@ -256,22 +256,22 @@ resource "null_resource" "dlt_pipeline_python_patch_policy_id" {
     "libraries": [
         {
             "notebook": {
-                "path": "/Repos/leoneller@yahoo.com.au/AWS-ISV-Summit/dlt/python/00_etl_dlt_bronze"
+                "path": "/Repos/${each.key}/AWS-ISV-Summit/dlt/python/00_etl_dlt_bronze"
             }
         },
         {
             "notebook": {
-                "path": "/Repos/leoneller@yahoo.com.au/AWS-ISV-Summit/utils/udfs"
+                "path": "/Repos/${each.key}/AWS-ISV-Summit/utils/udfs"
             }
         },
         {
             "notebook": {
-                "path": "/Repos/leoneller@yahoo.com.au/AWS-ISV-Summit/dlt/python/20_etl_dlt_gold"
+                "path": "/Repos/l${each.key}/AWS-ISV-Summit/dlt/python/20_etl_dlt_gold"
             }
         },
         {
             "notebook": {
-                "path": "/Repos/leoneller@yahoo.com.au/AWS-ISV-Summit/dlt/python/10_etl_dlt_silver"
+                "path": "/Repos/${each.key}/AWS-ISV-Summit/dlt/python/10_etl_dlt_silver"
             }
         }
     ],
